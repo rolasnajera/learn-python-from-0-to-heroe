b8b925c8815cf96f15de32f19b12518230cf0a4d
Rolas Najera <me@rolasnajera.dev>
2023-06-08 17:06:33 +0200

    Including the explanations, exercises and projects for day 5

---
+++ bootcamp/day5/explanations/loops.py
@@ -0,0 +1,8 @@
+# Use of the for loop
+
+fruits = ["apple", "banana", "cucumber"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+
+print(fruits)
---
+++ bootcamp/day5/projects/password-generator.py
@@ -0,0 +1,3 @@
+# Create a program that generates randome passwords
+# Ask to the user if they want to add simbols, and the lenght of the password.
+
